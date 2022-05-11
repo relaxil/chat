ed6167be84e294c410a35c9e15509462d1076a94
radmir <radmirkhaydarov12@gmail.com>
2022-05-12 00:53:38 +0300

    small addition

--- client/src/components/contacts/Contact.js
+++ client/src/components/contacts/Contact.js
@@ -1,6 +1,8 @@
 function Contact(props){
+  let activeUse = props.active === "active" ? 'contact active' : 'contact';
+  
   return (
-    <div className={props.active ==="active" ? 'contact active' : 'contact'}>
+    <div className={activeUse}>
       <div className="name">{props.name}</div>
       <div className="lastMessage">{props.lastMessage}</div>
     </div>
